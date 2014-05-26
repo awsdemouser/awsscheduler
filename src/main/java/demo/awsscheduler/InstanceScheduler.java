8fc7d05f37a3bfe4266343cdebe04c0098f5c166
awsdemouer <laszlo.lukacs@ericsson.com>
2014-05-26 10:42:26 +0200

    REF: replace magic number

--- src/main/java/demo/awsscheduler/InstanceScheduler.java
+++ src/main/java/demo/awsscheduler/InstanceScheduler.java
@@ -7,6 +7,7 @@ import java.util.ListIterator;
 
 public class InstanceScheduler implements InstanceKiller {
 
+	private static final int FIRST_ELEMENT = 0;
 	private final List<InstanceManager> instanceManagerStore;
 	private final InstanceManagerFactory instanceManagerFactory;
 	private final int queueLimit;
@@ -26,16 +27,16 @@ public class InstanceScheduler implements InstanceKiller {
 
 			if (instanceManagerIterator.hasNext()) {
 				final InstanceManager instanceManager = instanceManagerIterator.next();
-				while (!tasks.isEmpty() && instanceManager.addTask(tasks.get(0))) {
+				while (!tasks.isEmpty() && instanceManager.addTask(tasks.get(FIRST_ELEMENT))) {
 					tasks.remove(0);
 				}
 
 			} else {
 				final InstanceManager instanceManager = instanceManagerFactory.create(queueLimit, new AwsInstance(),
-						tasks.get(0), this);
+						tasks.get(FIRST_ELEMENT), this);
 				instanceManagerIterator.add(instanceManager);
 				instanceManagerIterator.previous();
-				tasks.remove(0);
+				tasks.remove(FIRST_ELEMENT);
 			}
 		}
 
